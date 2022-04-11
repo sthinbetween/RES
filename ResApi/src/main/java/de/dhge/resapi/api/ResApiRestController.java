e2d82be70abe80a5813349ba5b4478eddbd261a0
brandl <dane.brandl@navimatix.de>
2022-04-11 12:06:04 +0200

    integrated crud service into api logic

--- ResApi/src/main/java/de/dhge/resapi/api/ResApiRestController.java
+++ ResApi/src/main/java/de/dhge/resapi/api/ResApiRestController.java
@@ -1,40 +1,71 @@
 package de.dhge.resapi.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import de.dhge.resapi.ResApiApplication;
+import de.dhge.resapi.logic.ContentManager;
 
-
+@RestController
 public class ResApiRestController {
 
-	@GetMapping(value = "/version", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> version() {
-		return ResponseEntity.ok(ResApiApplication.version);
+	ContentManager contentManager;
+	
+	@Autowired
+	public ResApiRestController(ContentManager contentManager) {
+		this.contentManager = contentManager;
 	}
 	
-	
-	@GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> showList(){
+	@GetMapping(value = "/version", produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> version(@RequestParam(required=true) String secret) {
 		
-		return //TODO 
+		if(secret != null && secret.equals(ResApiApplication.secret)) {
+			return ResponseEntity.ok(ResApiApplication.version);
+		}
+		
+		else return ResponseEntity.badRequest().body("secret is false");
+		
+	}
+		
+	@GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> showList(@RequestParam(required=true) String secret){
+		
+		if(secret != null && secret.equals(ResApiApplication.secret)) {
+			return contentManager.showEntries();	
+		}
+		else return ResponseEntity.badRequest().body("secret is false");
 	}
 	
 	@PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> addEntry(@RequestParam(required=true) String entryTitle){
+	public ResponseEntity<?> addEntry(@RequestParam(required=true) String secret, @RequestParam(required=true) String entryTitle){
 		
-		return //TODO
+		if(secret != null && secret.equals(ResApiApplication.secret)) {
+			if(entryTitle != null && entryTitle.length() != 0) {
+				return contentManager.addEntry(entryTitle);
+			}
+			else return ResponseEntity.badRequest().body("no data was given");
+		}
+		else return ResponseEntity.badRequest().body("secret is false");
 	}
 	
 	@DeleteMapping(value = "/delete", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> deleteEntry(@RequestParam(required=false) String entryTitle){
+	public ResponseEntity<?> deleteEntry(@RequestParam(required=true) String secret, @RequestParam(required=false) String entryTitle, @RequestParam(required = false) Long entryOid){
 		
-		return //TOD
+		if(secret != null && secret.equals(ResApiApplication.secret)) {
+			if(entryTitle != null && entryTitle.length() != 0) {
+				return contentManager.deleteEntryTitle(entryTitle);
+			}
+			if(entryOid != null) {
+				return contentManager.deleteEntryOid(entryOid);
+			}
+			else return ResponseEntity.badRequest().body("no data was given");
+		}
+		else return ResponseEntity.badRequest().body("secret is false");
 	}
-	
-	
 }
--- ResApi/src/main/java/de/dhge/resapi/logic/ContentManager.java
+++ ResApi/src/main/java/de/dhge/resapi/logic/ContentManager.java
@@ -1,5 +1,109 @@
 package de.dhge.resapi.logic;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import de.dhge.resapi.model.TodoEntry;
+import de.dhge.resapi.service.TodoEntryRepository;
+import de.dhge.resapi.service.impl.TodoEntryService;
+
+@Service
 public class ContentManager {
+	
+	TodoEntryService todoEntryService;
+	
+	@Autowired
+	public ContentManager(TodoEntryService todoEntryService) {
+		this.todoEntryService = todoEntryService;
+	}
+	
+	
+	public ResponseEntity<?> addEntry(String title) {
+		
+		TodoEntry toAdd = new TodoEntry();
+		
+		toAdd.setEntryTitle(title);
+		toAdd.setCreationDate(LocalDateTime.now());
+		
+		TodoEntry added = todoEntryService.addEntry(toAdd);
+		
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("{\"added Entry\": [");
+		strBuilder.append(added.toJSONString());
+		strBuilder.append("]}");
+		
+		return ResponseEntity.ok(strBuilder.toString());	
+	}
+	
+	public ResponseEntity<?> deleteEntryTitle(String title) {
+		
+		ArrayList<TodoEntry> deleted = todoEntryService.deleteByTitle(title);
+		if(deleted != null) {
+			
+			StringBuilder strBuilder = new StringBuilder();
+			strBuilder.append("{\"deleted Entries\": [");
+			
+			for(TodoEntry x : deleted) {
+				strBuilder.append(x.toJSONString());
+				strBuilder.append(",");
+			}
+			
+			strBuilder.deleteCharAt(strBuilder.length() - 1);
+			strBuilder.append("]}");
+			
+			return ResponseEntity.ok(strBuilder.toString());
+		}
+		else {
+			return ResponseEntity.badRequest().body("no such Entry");
+		}
+		
+	}
+	
+	public ResponseEntity<?> deleteEntryOid(Long oid){
+		
+		TodoEntry deleted = todoEntryService.deleteByOid(oid);
+		if(deleted != null) {
+			
+			StringBuilder strBuilder = new StringBuilder();
+			strBuilder.append("{\"deleted Entry\": [");
+			strBuilder.append(deleted.toJSONString());
+			strBuilder.append("]}");
+			
+			return ResponseEntity.ok(strBuilder.toString());
+		}
+		else {
+			return ResponseEntity.badRequest().body("no such Entry");
+		}
+		
+	}
+	
+	public ResponseEntity<?> showEntries(){
+		
+		ArrayList<TodoEntry> allEntries = todoEntryService.findAll();
+		if(allEntries != null) {
+			
+			StringBuilder strBuilder = new StringBuilder();
+			strBuilder.append("{\"all Entries\": [");
+			
+			for(TodoEntry x : allEntries) {
+				strBuilder.append(x.toJSONString());
+				strBuilder.append(",");
+			}
+			
+			strBuilder.deleteCharAt(strBuilder.length() - 1);
+			strBuilder.append("]}");
+			
+			return ResponseEntity.ok(strBuilder.toString());
+		}
+		else {
+			return ResponseEntity.badRequest().body("no Entries");
+		}
+		
+		
+	}
 
 }
