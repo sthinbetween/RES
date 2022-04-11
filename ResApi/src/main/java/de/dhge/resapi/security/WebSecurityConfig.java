11e52da5fde34e776247322299259c78d5d130b5
brandl <dane.brandl@navimatix.de>
2022-04-11 12:05:08 +0200

    updated sercurity config to allow endpoints

--- ResApi/src/main/java/de/dhge/resapi/security/WebSecurityConfig.java
+++ ResApi/src/main/java/de/dhge/resapi/security/WebSecurityConfig.java
@@ -1,5 +1,28 @@
 package de.dhge.resapi.security;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+		  prePostEnabled = true, 
+		  jsr250Enabled = true,
+		  proxyTargetClass = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests()
+        	.antMatchers("/version", "/list", "/add", "/delete").permitAll()
+       	
+            .anyRequest().authenticated()
+            .and().httpBasic();
+        
+        http.headers().frameOptions().disable(); //for H2
+    }
 }
