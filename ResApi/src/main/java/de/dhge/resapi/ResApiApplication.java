0d657a94c8b77e153cbf90f2794090acc1569a52
brandl <dane.brandl@navimatix.de>
2022-04-11 12:02:12 +0200

    added secret for api requests

--- ResApi/src/main/java/de/dhge/resapi/ResApiApplication.java
+++ ResApi/src/main/java/de/dhge/resapi/ResApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResApiApplication {
 	
 	public static String version = "0.0.1";
+	public static String secret = "asd45jASBD73-asdd3dfASd-!asF3";
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResApiApplication.class, args);
