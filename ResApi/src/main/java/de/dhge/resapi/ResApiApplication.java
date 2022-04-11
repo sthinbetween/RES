e9cc73a472b3c193fe48ebf5fbb7bb87f077accf
brandl <dane.brandl@navimatix.de>
2022-04-11 10:48:07 +0200

    added version

--- ResApi/src/main/java/de/dhge/resapi/ResApiApplication.java
+++ ResApi/src/main/java/de/dhge/resapi/ResApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResApiApplication {
+	
+	public static String version = "0.0.1";
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResApiApplication.class, args);
