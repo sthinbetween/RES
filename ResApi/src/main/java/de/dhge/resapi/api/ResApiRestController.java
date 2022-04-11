cfd359c9e4e44d5734cf4f5c89a887d15a083677
brandl <dane.brandl@navimatix.de>
2022-04-11 10:49:54 +0200

    created api endpoints

--- ResApi/src/main/java/de/dhge/resapi/api/ResApiRestController.java
+++ ResApi/src/main/java/de/dhge/resapi/api/ResApiRestController.java
@@ -1,5 +1,40 @@
 package de.dhge.resapi.api;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import de.dhge.resapi.ResApiApplication;
+
+
 public class ResApiRestController {
 
+	@GetMapping(value = "/version", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> version() {
+		return ResponseEntity.ok(ResApiApplication.version);
+	}
+	
+	
+	@GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> showList(){
+		
+		return //TODO 
+	}
+	
+	@PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> addEntry(@RequestParam(required=true) String entryTitle){
+		
+		return //TODO
+	}
+	
+	@DeleteMapping(value = "/delete", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> deleteEntry(@RequestParam(required=false) String entryTitle){
+		
+		return //TOD
+	}
+	
+	
 }
