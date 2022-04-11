5ff6d3536cdd7bb90a9840279d469a8afe0d69e6
brandl <dane.brandl@navimatix.de>
2022-04-11 12:04:13 +0200

    implemented crud operations as service

--- ResApi/src/main/java/de/dhge/resapi/service/TodoEntryRepository.java
+++ ResApi/src/main/java/de/dhge/resapi/service/TodoEntryRepository.java
@@ -1,5 +1,17 @@
 package de.dhge.resapi.service;
 
-public class TodoEntryRepository {
+import java.util.ArrayList;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import de.dhge.resapi.model.TodoEntry;
+
+public interface TodoEntryRepository extends JpaRepository<TodoEntry, Long>{
+
+	public TodoEntry findByOid(Long oid);
+	
+	public ArrayList<TodoEntry> findAll();
+	
+	public ArrayList<TodoEntry> findByEntryTitle(String entryTitle);
+		
 }
--- ResApi/src/main/java/de/dhge/resapi/service/impl/TodoEntryService.java
+++ ResApi/src/main/java/de/dhge/resapi/service/impl/TodoEntryService.java
@@ -1,5 +1,57 @@
 package de.dhge.resapi.service.impl;
 
-public class TodoEntryService {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.dhge.resapi.model.TodoEntry;
+import de.dhge.resapi.service.TodoEntryRepository;
+
+@Service
+public class TodoEntryService {
+	
+	@Autowired
+	TodoEntryRepository todoEntryRepository;
+
+	public TodoEntry findByOid(Long oid) {
+		return  todoEntryRepository.findByOid(oid);
+	}
+	
+	public ArrayList<TodoEntry> findAll(){
+		return todoEntryRepository.findAll();
+	}
+	
+	public ArrayList<TodoEntry> findByEntryTitle(String entryTitle){
+		return todoEntryRepository.findByEntryTitle(entryTitle);
+	}
+	
+	public ArrayList<TodoEntry> deleteByTitle(String entryTitle) {
+		
+		ArrayList<TodoEntry> toDelete = todoEntryRepository.findByEntryTitle(entryTitle);
+		
+		if(toDelete.size() != 0) {
+			for(TodoEntry x : toDelete) {
+				todoEntryRepository.delete(x);
+			}
+			return toDelete;
+		}
+		else return null;
+		
+	}
+	
+	public TodoEntry deleteByOid(Long oid) {
+		
+		TodoEntry toDelete = todoEntryRepository.findByOid(oid);
+		
+		if(toDelete != null) {
+			todoEntryRepository.delete(toDelete);
+			return toDelete;
+		}
+		else return null;
+	}
+	
+	public TodoEntry addEntry(TodoEntry entry) {
+		return todoEntryRepository.save(entry);
+	}
 }
