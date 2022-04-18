2c2641b72433d09ac697f761faaca312cb4e59f5
sthinbetween <58440199+sthinbetween@users.noreply.github.com>
2022-04-18 12:02:44 +0200

    Update WebSecurityConfig.java

--- ResApi/src/main/java/de/dhge/resapi/security/WebSecurityConfig.java
+++ ResApi/src/main/java/de/dhge/resapi/security/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         	.antMatchers("/version", "/list", "/add", "/delete").permitAll()
        	
             .anyRequest().authenticated()
-            .security.httpBasic().disable();;
+            .security.httpBasic().disable();
         
         http.headers().frameOptions().disable(); //for H2
     }
