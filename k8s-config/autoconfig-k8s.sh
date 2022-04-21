cc50599c5702631d73ab2bd0dbd6d1f036a6e891
sthinbetween <58440199+sthinbetween@users.noreply.github.com>
2022-04-21 12:49:47 +0200

    Update autoconfig-k8s.sh

--- k8s-config/autoconfig-k8s.sh
+++ k8s-config/autoconfig-k8s.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-# Notwendige Repos hinzufügen
+# Farben definieren
 GREEN='\033[1;32m'
 NC='\033[0m'
+# Helm-Repos hinzufügen
 echo -e "${GREEN}Füge Helm-Repositories hinzu...${NC}"
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add haproxytech https://haproxytech.github.io/helm-charts
 helm repo update
 echo -e "${GREEN}Repositories hinzugefügt...${NC}"
+# Konfigurationsdateien herunterladen
+FILE=./galera-helm.yaml
+if test -f "$FILE"; then
+    echo -e "${GREEN}Galera-Konfig vorhanden, überspringe.${NC}"
+else
+   wget https://github.com/sthinbetween/RES/blob/357513884bbb389318894d788fca59041b9e941f/k8s-config/galera-helm.yaml 
+   echo -e "${GREEN}Galera-Konfig geladen${NC}"
+fi
+FILE=./todoapi-deployment.yaml
+if test -f "$FILE"; then
+    echo -e "${GREEN}API-Konfig vorhanden, überspringe.${NC}"
+else
+   wget https://github.com/sthinbetween/RES/blob/357513884bbb389318894d788fca59041b9e941f/k8s-config/galera-helm.yaml
+   echo -e "${GREEN}API-Konfig geladen${NC}"
+fi
+echo -e "${GREEN}Konfigurationen geladen...${NC}"
 echo -e "${GREEN}Installiere Galera-Helm Chart...${NC}"
 helm install maria-db -f ./galera-helm.yaml bitnami/mariadb-galera
 echo -e "${GREEN}Galera-Helm Chart installiert...${NC}"
